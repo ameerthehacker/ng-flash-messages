cc8fba9be65bed3540a333e003097386d60b3259
ameerthehacker <ameerjhanprof@gmail.com>
2017-11-08 18:42:20 +0530

    Add animation for making the alert invisible

--- src/components/ng-flash-message.component.ts
+++ src/components/ng-flash-message.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { 
+    state, 
+    trigger, 
+    style,
+    transition,
+    animate
+} from '@angular/animations';
 
 @Component({
     selector: 'ng-flash-message',
     templateUrl: './ng-flash-message.component.html',
-    styleUrls: ['./styles.scss']
+    styleUrls: ['./styles.scss'],
+    animations: [
+        trigger('alertState', [
+            state('visible', style({
+                opacity: 1
+            })),
+            state('invisible', style({
+                opacity: 0
+            })),
+            transition('visible => invisible', animate('1000ms ease-in')),
+            transition('invisible => visible', animate('0ms linear'))
+        ])
+    ]
 })
 export class NgFlashMessageComponent {
 
-    alertVisible: boolean = true;
+    alertVisibility: string = 'visible';
 
     onBtnCloseClick(evt) {
         evt.preventDefault();
-        this.alertVisible = false;
+        this.alertVisibility = 'invisible';
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgFlashMessageComponent } from './components/ng-flash-message.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     NgFlashMessageComponent
